c941cab0b86325c93363b609f4aff84a300ad4cf
suryabot-123 <90522683+suryabot-123@users.noreply.github.com>
2021-09-12 00:03:28 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -19,11 +19,11 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+*Creator number : https://wa.link/1cuosr*
+
+*Githublink (Setup)  :    https://github.com/suryabot-123/surya*
+
+
 `}) 
 
 }));
