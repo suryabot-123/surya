33a6cf0fac926c3bfb98a6ee630c2fcef84a1208
suryabot-123 <90522683+suryabot-123@users.noreply.github.com>
2021-09-13 22:40:13 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -39,7 +39,7 @@ else if (Config.WORKTYPE == 'public') {
             await message.client.sendMessage(message.jid,'Pikachu Bot created by *Surya*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS*', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS* Surya is my creator', MessageType.text);
         }
     }));
 }
