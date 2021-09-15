66e6d286bb1e79566ca5efae2dff10d7d8f93c38
suryabot-123 <90522683+suryabot-123@users.noreply.github.com>
2021-09-15 23:53:08 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -36,10 +36,10 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'*Pikachu bot created by Surya 💞* : http://wa.me//+918547685581' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*Pikachu bot created by Surya 💞* ɪͨᴛʸsᵇ ͤᴍͬᴇ ͩsͦᴜͫʀͤʏᴀ : http://wa.me//+918547685581' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.PLK + '👈🏻He is my Owner,My creator is Surya💞:http://wa.me//+918547685581', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.PLK + '👈🏻He is my Owner,My creator is Surya💞 ɪͨᴛʸsᵇ ͤᴍͬᴇ ͩsͦᴜͫʀͤʏᴀ:http://wa.me//+918547685581', MessageType.text);
         }
     }));
 }
