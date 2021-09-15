97eb801a723a79313a1eb177b968f2203cba5e93
suryabot-123 <90522683+suryabot-123@users.noreply.github.com>
2021-09-16 00:57:24 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -133,7 +133,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
         });
 
         console.log(
-            chalk.green.bold('Thakudu Running! 😎')
+            chalk.green.bold('surya Running! 😎')
         );
     });
     
