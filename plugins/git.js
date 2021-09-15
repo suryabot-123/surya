d41885d010ff6fd46042666123e24aeb89f63723
suryabot-123 <90522683+suryabot-123@users.noreply.github.com>
2021-09-15 23:39:17 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Surya*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Surya💞*
 *Creator number : https://wa.link/1cuosr*
 
 *Githublink (Setup)  :    https://github.com/suryabot-123/surya*
