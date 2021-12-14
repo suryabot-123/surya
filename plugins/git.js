436a30aa9230a8c3d0824b93fb0fc5d1fb790d41
suryabot-123 <90522683+suryabot-123@users.noreply.github.com>
2021-12-14 16:43:44 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "hacker.png";
+  r_text[0] = "https://images.app.goo.gl/1PbG1YgFdLJ9RvKq9";
     
     
     var i = Math.floor(1*Math.random())
